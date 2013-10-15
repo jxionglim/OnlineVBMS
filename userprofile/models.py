a5d864b377b827433926095633bb250e855ece93
jxionglim <jxiong.lim@gmail.com>
2013-10-15 12:21:22 +0800

    fix form

--- userprofile/models.py
+++ userprofile/models.py
@@ -2,27 +2,39 @@ from django import forms
 from django.db import models
 from django.forms import ModelForm
 
+
 class Customer(models.Model):
-	cusId = models.IntegerField()
-	firstName = models.CharField(max_length=256)
-	lastName = models.CharField(max_length=256)
-	contactNo = models.BigIntegerField()
-	email = models.EmailField(max_length=256)
-	zipcode = models.BigIntegerField()
-	streetName = models.CharField(max_length=256)
-	cExpDate = models.DateField()
-	cSerialNo = models.BigIntegerField()
-	password = models.CharField(max_length=256)
-	
-	class Meta:
-		managed = False
-		
-class RegisterForm(ModelForm):
+    cusId = models.IntegerField()
+    firstName = models.CharField(max_length=256)
+    lastName = models.CharField(max_length=256)
+    contactNo = models.BigIntegerField()
+    email = models.EmailField(max_length=256)
+    zipcode = models.BigIntegerField()
+    streetName = models.CharField(max_length=256)
+    cExpDate = models.DateField()
+    cSerialNo = models.BigIntegerField()
+    password = models.CharField(max_length=256)
+
     class Meta:
-		model = Customer
-		exclude = ['cusId']
-		widgets = {'password': forms.PasswordInput()}
-		
+        managed = False
+
+
+class RegisterForm(ModelForm):
+    firstName = forms.CharField(max_length=256)
+    lastName = forms.CharField(max_length=256)
+    #contactNo = forms.IntegerField()
+    email = forms.EmailField(max_length=256)
+    #zipcode = forms.IntegerField()
+    streetName = forms.CharField(max_length=256)
+    cExpDate = forms.DateField()
+    #cSerialNo = forms.IntegerField()
+    password = forms.CharField(widget=forms.PasswordInput(), max_length=256)
+
+    class Meta:
+        model = Customer
+        exclude = ['cusId']
+
+
 class LoginForm(forms.Form):
     email = forms.EmailField(max_length=256,label="Email")
     password = forms.CharField(max_length=256,widget=forms.PasswordInput)
--- userprofile/views.py
+++ userprofile/views.py
@@ -1,17 +1,44 @@
-from django.db import connection
+from django.db import connection, transaction
 from django.template import RequestContext
-from django.http import HttpResponseRedirect, HttpResponse
-from django.shortcuts import render_to_response, get_object_or_404, redirect
-from django.core.urlresolvers import reverse
-from userprofile.models import RegisterForm
+from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response
+from userprofile.models import RegisterForm, Customer
 
-# Create your views here.
+
 def register(request):
-	return render_to_response('userprofile/register.html', {
-		'form' : RegisterForm(),
-		},context_instance=RequestContext(request))
-		
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            cursor = connection.cursor()
+            query = 'SELECT MAX(cusId) FROM customer'
+            cursor.execute(query)
+            row = cursor.fetchone()
+            cusId = row[0]+1 if row[0] is not None else 1
+            query = "INSERT INTO customer VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+            params = [
+                cusId,
+                request.POST['firstName'],
+                request.POST['lastName'],
+                request.POST['email'],
+                request.POST['contactNo'],
+                request.POST['zipcode'],
+                request.POST['streetName'],
+                request.POST['cExpDate'],
+                request.POST['cSerialNo'],
+                request.POST['password']
+            ]
+            cursor.execute(query, params)
+            transaction.commit_unless_managed()
+            return HttpResponseRedirect('/register')
+    else:
+        form = RegisterForm()
+
+    return render_to_response('userprofile/register.html', {
+        'form': form
+    }, context_instance=RequestContext(request))
+
+
 def viewProfile(request):
-	return render_to_response('userprofile/register.html', {
-		'form' : RegisterForm(),
-		},context_instance=RequestContext(request))
+    return render_to_response('userprofile/register.html', {
+        'form' : RegisterForm(),
+        },context_instance=RequestContext(request))
