6d4d57d9b5cbae3ba1ce2fbc2de72f787904d7cc
jxionglim <jxiong.lim@gmail.com>
2013-10-17 20:30:36 +0800

    changed url

--- OnlineVBMS/urls.py
+++ OnlineVBMS/urls.py
@@ -2,13 +2,8 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     (r'^$', 'userprofile.views.login'),
-<<<<<<< HEAD
+	(r'^logout$', 'userprofile.views.logout'),
     (r'^register$', 'userprofile.views.register'),
     (r'^customer/', include('customer.urls')),
     (r'^user/', include('userprofile.urls')),
-=======
-	(r'^register$', 'userprofile.views.register'),
-	(r'^user/', include('userprofile.urls')),
-    (r'^admin/', include('admin.urls')),
->>>>>>> 6b4e8a0fc751403b6fe8771f3570c441986ab33e
-)
+
