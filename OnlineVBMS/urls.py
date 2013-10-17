e4aa60c91ffe8dd5fd4ea119869b0affd551c1d3
jxionglim <jxiong.lim@gmail.com>
2013-10-17 20:34:04 +0800

    fixed url

--- OnlineVBMS/urls.py
+++ OnlineVBMS/urls.py
@@ -2,8 +2,9 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     (r'^$', 'userprofile.views.login'),
-	(r'^logout$', 'userprofile.views.logout'),
+    (r'^logout$', 'userprofile.views.logout'),
     (r'^register$', 'userprofile.views.register'),
     (r'^customer/', include('customer.urls')),
     (r'^user/', include('userprofile.urls')),
+    )
 
